a59dd0cfd74fe5166cb43fe06bdd24fdd1ef72fe
Ruben van der Peet <Ruben.vanderpeet@outlook.com>
2025-12-09 15:46:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-// JavaScript Document
-console.log("hi");
+// --- Constanten ---
+
+
+// --- Variabelen ---
+
+let playButton = document.querySelector('.playbutton')
+let playButtonStatus = false
+let icoonPlayButton = document.querySelector('.svgiconplay')
+let frontPageVideo = document.querySelector('video')
+
+// --- Functies ---
+
+
+
+//Funtie die de rallylichten verandert (boolean)
+function videoPauzeren(){
+
+    if (playButtonStatus){
+
+        icoonPlayButton.src = 'https://cdn.ui.porsche.com/porsche-design-system/icons/pause.e41b935.svg'
+        playButtonStatus = false
+        frontPageVideo.play()
+    } 
+    else {
+
+        icoonPlayButton.src = 'https://cdn.ui.porsche.com/porsche-design-system/icons/play.24226d4.svg'
+        playButtonStatus = true
+        frontPageVideo.pause()
+    }
+  
+}
+
+
+// --- Event listeners ---
+
+
+
+//koppeling van de constanten aan de functies koppelt
+playButton.addEventListener('click', videoPauzeren)
